3bd07abf28dbf3c2073e1a8aa0c4bee1229e3b05
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2021-04-18 16:44:42 -0300

    instalando e fazendo primeiro uso do miragejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import {createServer} from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'teclado mecanico',
+          amount: 400,
+          type: 'withdraw',
+          category: 'compras',
+          createdAt: new Date()
+        }
+      ]
+    });
+  }
+})
 
 export function App() {
   return (
