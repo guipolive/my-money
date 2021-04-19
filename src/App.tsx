18ff66b29ebcff1bc95a1df04c84f2c40b85783c
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2021-04-19 16:32:50 -0300

    transferindo modal para o App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import {createServer} from 'miragejs';
+import { useState } from 'react';
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root'); // Por questões de acessibilidade, dizemos ao Modal qual é o elemento principal da aplicação. Com isso, o elemento principal diz ao usuário que está com acessibilidade ligada que essa parte principal da aplicação não está acessível, pois o modal está aberto.
 
 createServer({
   routes() {
@@ -23,10 +27,30 @@ createServer({
 })
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+	function handleOpenNewTransactionModal() {
+		setIsNewTransactionModalOpen(true);
+	}
+	
+	function handleCloseNewTransactionModal() {
+		setIsNewTransactionModalOpen(false);
+	}
+
   return (
     <>
-      <Header />
+      <Header 
+        onOpenNewTransactionModal={handleOpenNewTransactionModal}
+      />
       <Dashboard />
+
+      <Modal
+					isOpen={isNewTransactionModalOpen}
+					onRequestClose={handleCloseNewTransactionModal}
+				>
+        <h2>cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyle/>
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react';
-import Modal from 'react-modal'
 import logoImg from '../../assets/logo-group.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+	onOpenNewTransactionModal: () => void;
+}
 
-	const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-
-	function handleOpenNewTransactionModal() {
-		setIsNewTransactionModalOpen(true);
-	}
-	
-	function handleCloseNewTransactionModal() {
-		setIsNewTransactionModalOpen(false);
-	}
+export function Header({onOpenNewTransactionModal}: HeaderProps) {
 
 	return(
 		<Container>
 			<Content>
 				<img src={logoImg} alt="my money"/>
-				<button type="button" onClick={handleOpenNewTransactionModal}>
+				<button type="button" onClick={onOpenNewTransactionModal}>
 					nova transação
 				</button>
 
-				<Modal
-					isOpen={isNewTransactionModalOpen}
-					onRequestClose={handleCloseNewTransactionModal}
-				>
-					<h2>cadastrar transação</h2>
-				</Modal>
+				
 			</Content>
 		</Container>
 	)
