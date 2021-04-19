00ea8e57f94b222f058f39133f21547da4d36847
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2021-04-19 19:07:20 -0300

    adicionando seeds e exibindo as transactions na TransactionsTable

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,30 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Venda de website',
+          type: 'deposit',
+          category: 'pj',
+          amount: 3000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        
+        {
+          id: 2,
+          title: 'Compra do monitor',
+          type: 'withdraw',
+          category: 'compras online',
+          amount: 1200,
+          createdAt: new Date('2021-02-12 11:00:00')
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
