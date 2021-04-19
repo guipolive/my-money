03381b3b8e37b1689d1b7bc7a8b27f154f215f89
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2021-04-19 18:49:05 -0300

    att miragejs structure

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,11 @@
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal'
 import { Container, TransactionTypeContainer, TypeBox } from './styles';
 import closeImg from '../../assets/x.svg';
 
 import incomeImg from '../../assets/entradas.svg';
 import outcomeImg from '../../assets/saidas.svg';
-import { useState } from 'react';
+import { api } from '../../services/api';
 
 interface NewTransactionModalProps {
 	isOpen: boolean;
@@ -14,7 +15,24 @@ interface NewTransactionModalProps {
 Modal.setAppElement('#root'); // Por questões de acessibilidade, dizemos ao Modal qual é o elemento principal da aplicação. Com isso, o elemento principal diz ao usuário que está com acessibilidade ligada que essa parte principal da aplicação não está acessível, pois o modal está aberto.
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+	const [title, setTitle] = useState('');
+	const [value, setValue] = useState(0);
+	const [category, setCategory] = useState('');
+
 	const [type, setType] = useState('deposit');
+
+	function handleCreateNewTransaction(event: FormEvent) {
+		event.preventDefault(); // previne o carregamento padrão do submit
+
+		const data = ({
+			title,
+			value,
+			category,
+			type
+		});
+
+		api.post('/transactions', data);
+	}
 
 	return(
 		<Modal
@@ -32,16 +50,20 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 				<img src={closeImg} alt="fechar modal"/>
 			</button>
 
-			<Container>
+			<Container onSubmit={handleCreateNewTransaction} >
 				<h2>cadastrar transação</h2>
 
 				<input 
 					placeholder="título"
+					value={title}
+					onChange={event => setTitle(event.target.value)}
 				/>
 
 				<input 
 					type="number"
 					placeholder="valor"
+					value={value}
+					onChange={event => setValue(+event.target.value)}
 				/>
 
 				<TransactionTypeContainer>
@@ -69,6 +91,8 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 
 				<input 
 					placeholder="categoria"
+					value={category}
+					onChange={event => setCategory(event.target.value)}
 				/>
 
 				<button type="submit">
